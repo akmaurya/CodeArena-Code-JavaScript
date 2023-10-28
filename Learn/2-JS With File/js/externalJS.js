810863ff62263549d59f06f47c3c655ed4970a5d
Arvind Kumar Maurya <arvindmaurya1905@gmail.com>
2023-10-28 21:37:05 +0530

    Initial Commit

---
+++ Learn/2-JS With File/js/externalJS.js
@@ -0,0 +1,6 @@
+document.write("Hello World form js file<br>");//simeple Stirng which will be pring on html page
+document.write("<i><b>Hello World Next Line</b> from js file</i>");
+/*
+    This is example of multiple line comment
+    Same as java.
+*/
